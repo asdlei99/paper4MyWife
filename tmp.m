72e474558b0b46f3852cfc7f83638239ae62e206
czyt1988 <czy.t@163.com>
2017-06-03 22:24:33 +0800

    符号计算

---
+++ tmp.m
@@ -0,0 +1,32 @@
+L = 10;
+Dpipe = 0.106;
+a = 345;
+isDamping = 0;
+coeffFriction = 0;
+meanFlowVelocity = 14.5;
+mach = meanFlowVelocity / a;
+notMach = 0;
+calcWay2 = 0;
+density = 1.293;
+dynViscosity = nan;
+
+
+matrix_straigh = straightPipeTransferMatrixFreSymbol(L,'frequency','a',a,'D',Dpipe...
+        ,'isDamping',isDamping,'coeffFriction',coeffFriction,'meanFlowVelocity',meanFlowVelocity...
+        ,'m',mach,'notmach',notMach...
+        ,'calcWay2',calcWay2,'density',density,'dynViscosity',dynViscosity);
+matrix_straigh = matrix_straigh * [1;0];
+syms frequency
+A = matrix_straigh(1);
+B = matrix_straigh(2);
+equ = A == 0;
+ans = solve(equ,frequency);
+eval(ans)
+
+equ = B == 1;
+ans = solve(equ,frequency);
+eval(ans)
+
+
+
+
