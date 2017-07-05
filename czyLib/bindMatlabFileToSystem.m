9a34865652aa44b8e2f1525e8b8444b8e661e655
czyt1988 <czy.t@163.com>
2017-07-05 22:53:44 +0800

    添加数据读取

--- czyLib/bindMatlabFileToSystem.m
+++ czyLib/bindMatlabFileToSystem.m
@@ -1,7 +1,14 @@
 function bindMatlabFileToSystem
-    cwd=pwd;
-    cd([matlabroot '\toolbox\matlab\winfun\private']);
-    fileassoc('add',{'.m','.mat','.fig','.p','.mdl',['.' mexext]}); %重点
-    cd(cwd);
-    disp('Changed Windows file associations. FIG, M, MAT, MDL, MEX, and P files are now associated with MATLAB.')
+
+%     cwd=pwd;
+%     cd([matlabroot '\toolbox\matlab\winfun\private']);
+%     fileassoc('add',{'.m','.mat','.fig','.p','.mdl',['.' mexext]}); %重点
+%     cd(cwd);
+%     disp('Changed Windows file associations. FIG, M, MAT, MDL, MEX, and P files are now associated with MATLAB.')
+%     
+    cwd=pwd;  
+    cd([matlabroot '\mcr\toolbox\matlab\winfun\private']);  
+    fileassoc('add',{'.mat','.fig','.p','.mdl',['.' mexext]});  
+    cd(cwd);  
+    disp('Changed Windows file associations. FIG, M, MAT, MDL, MEX, and P files are now associated with MATLAB.');  
 end
--- gitRemoveLargeFile.sh
+++ gitRemoveLargeFile.sh
@@ -1,1 +1,2 @@
 git verify-pack -v .git/objects/pack/*.idx | sort -k 3 -n | tail -5 | awk '{print$1}'
+git rev-list --objects --all | grep 1abdfasd
