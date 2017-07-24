cf3e50e0ffec6373dbf85be67c66073c9c09b2e4
czyt1988 <czy.t@163.com>
2017-07-24 16:05:33 +0800

    Signed-off-by: mqy <15001395919@163.com> Signed-off-by: czyt1988 <czy.t@163.com>

--- figure/figureExpNatureFrequency.m
+++ figure/figureExpNatureFrequency.m
@@ -1,131 +1,205 @@
 function fh = figureExpNatureFrequency(dataCombineStructCells,varargin)
-%ªÊ÷∆ µ—È ˝æ›µƒ—π¡¶¬ˆ∂Ø∫Õ“÷÷∆¬ Õº
+%ÁªòÂà∂ÂÆûÈ™åÊï∞ÊçÆÁöÑÂéãÂäõËÑâÂä®ÂíåÊäëÂà∂ÁéáÂõæ
 pp = varargin;
-errorType = 'ci';%ªÊ÷∆ŒÛ≤Ó¥¯µƒƒ£ Ω£¨std£∫mean+-sd,ciŒ™95%÷√–≈«¯º‰£¨minmaxŒ™◊Ó¥Û◊Ó–°
-rang = 1:13;
-yFilterFunPtr = nan;
-legendLabels = {};
-baseField = 'rawData';
-showPureVessel = 0;
-rpm = 420;
-natureFre= [1,2];%πÃ”–∆µ¬ £¨÷ß≥÷[0.5,1,1.5,2,2.5,3]
-%‘ –ÌÃÿ ‚µƒ∞—µÿ“ª∏ˆvarargin◊˜Œ™legend
+chartType = 'line';
+varargin = {};
+%ÂÖÅËÆ∏ÁâπÊÆäÁöÑÊääÂú∞‰∏Ä‰∏™varargin‰Ωú‰∏∫legend
 if 0 ~= mod(length(pp),2)
-    legendLabels = pp{1};
+    varargin{1} = pp{1};
     pp=pp(2:end);
 end
+
 while length(pp)>=2
     prop =pp{1};
     val=pp{2};
     pp=pp(3:end);
     switch lower(prop)
-        case 'errortype' %ŒÛ≤Ó¥¯µƒ¿‡–Õ
-        	errorType = val;
-        case 'rang'
-            rang = val;
-        case 'yfilterfunptr'
-            yFilterFunPtr = val;
-        case 'basefield'
-            baseField = val;
-        case 'naturefre'
-            natureFre = val;
-        case 'nf'
-            natureFre = val;
-        case 'showpurevessel'
-            showPureVessel = val;
-        case 'rpm'
-            rpm = val;
+        case 'chartType' %ÁªòÂõæÁ±ªÂûãÂèØÈÄâ'line'Âíå'bar'
+            chartType = val;
         otherwise
-       		error('≤Œ ˝¥ÌŒÛ%s',prop);
+            varargin{length(varargin)+1} = prop;
+            varargin{length(varargin)+1} = val;
     end
+end
+if strcmp(chartType,'line')
+    fh = plotInLine(dataCombineStructCells,varargin{:});
+else
+    fh = plotInBar(dataCombineStructCells,varargin{:});
+end
 end
 
 
-fh.figure = figure;
-paperFigureSet_normal();
-x = constExpMeasurementPointDistance();%≤‚µ„∂‘”¶µƒæ‡¿Î
-%–Ë“™œ‘ æµ•“ªª∫≥Âπﬁ
-
-if showPureVessel
-    dataPat = getPureVesselCombineDataPath(rpm);
-    st = loadExpCombineDataStrcut(dataPat);
-    for i=1:length(natureFre)
-        plotLineStyle = getLineStyle(i);
-        [y,stdVal,maxVal,minVal,muci] = getExpCombineNatureFrequencyDatas(st,natureFre(i),baseField);
-        [yUp,yDown] = getUpDownRang(y,stdVal,maxVal,minVal,muci,errorType,rang);
-        y = y(rang);
-        if strcmp(errorType,'none')
-            fh.vesselHandle(i) = plot(x,y,'color',[160,162,162]./255 ...
-                ,'LineStyle',plotLineStyle');
-        else
-            [fh.vesselHandle(i),fh.vesselErrFillHandle(i)] = plotWithError(x,y,yUp,yDown...
-                ,'color',[160,162,162]./255 ...
-                ,'LineStyle',plotLineStyle);
+function fh = plotInLine(dataCombineStructCells,varargin)
+    pp = varargin;
+    errorType = 'ci';%ÁªòÂà∂ËØØÂ∑ÆÂ∏¶ÁöÑÊ®°ÂºèÔºåstdÔºömean+-sd,ci‰∏∫95%ÁΩÆ‰ø°Âå∫Èó¥Ôºåminmax‰∏∫ÊúÄÂ§ßÊúÄÂ∞è
+    rang = 1:13;
+    yFilterFunPtr = nan;
+    legendLabels = {};
+    baseField = 'rawData';
+    showPureVessel = 0;
+    rpm = 420;
+    natureFre= [1,2];%Âõ∫ÊúâÈ¢ëÁéáÔºåÊîØÊåÅ[0.5,1,1.5,2,2.5,3]
+    %ÂÖÅËÆ∏ÁâπÊÆäÁöÑÊääÂú∞‰∏Ä‰∏™varargin‰Ωú‰∏∫legend
+    if 0 ~= mod(length(pp),2)
+        legendLabels = pp{1};
+        pp=pp(2:end);
+    end
+    while length(pp)>=2
+        prop =pp{1};
+        val=pp{2};
+        pp=pp(3:end);
+        switch lower(prop)
+            case 'errortype' %ËØØÂ∑ÆÂ∏¶ÁöÑÁ±ªÂûã
+                errorType = val;
+            case 'rang'
+                rang = val;
+            case 'yfilterfunptr'
+                yFilterFunPtr = val;
+            case 'basefield'
+                baseField = val;
+            case 'naturefre'
+                natureFre = val;
+            case 'nf'
+                natureFre = val;
+            case 'showpurevessel'
+                showPureVessel = val;
+            case 'rpm'
+                rpm = val;
+            otherwise
+                error('ÂèÇÊï∞ÈîôËØØ%s',prop);
         end
     end
+
+
+    fh.figure = figure;
+    paperFigureSet_normal();
+    x = constExpMeasurementPointDistance();%ÊµãÁÇπÂØπÂ∫îÁöÑË∑ùÁ¶ª
+    %ÈúÄË¶ÅÊòæÁ§∫Âçï‰∏ÄÁºìÂÜ≤ÁΩê
+
+    if showPureVessel
+        dataPat = getPureVesselCombineDataPath(rpm);
+        st = loadExpCombineDataStrcut(dataPat);
+        for i=1:length(natureFre)
+            plotLineStyle = getLineStyle(i);
+            [y,stdVal,maxVal,minVal,muci] = getExpCombineNatureFrequencyDatas(st,natureFre(i),baseField);
+            [yUp,yDown] = getUpDownRang(y,stdVal,maxVal,minVal,muci,errorType,rang);
+            y = y(rang);
+            if strcmp(errorType,'none')
+                fh.vesselHandle(i) = plot(x,y,'color',[160,162,162]./255 ...
+                    ,'LineStyle',plotLineStyle');
+            else
+                [fh.vesselHandle(i),fh.vesselErrFillHandle(i)] = plotWithError(x,y,yUp,yDown...
+                    ,'color',[160,162,162]./255 ...
+                    ,'LineStyle',plotLineStyle);
+            end
+        end
+        
+        hold on;
+    end
+
+    for plotCount = 1:length(dataCombineStructCells)
+        plotMarker = getMarkStyle(plotCount);
+        plotColor = getPlotColor(plotCount);
+        
+        for i=1:length(natureFre)
+            plotLineStyle = getLineStyle(i);
+            if 1==length(dataCombineStructCells)
+                [y,stdVal,maxVal,minVal,muci] = getExpCombineNatureFrequencyDatas(dataCombineStructCells,natureFre(i),baseField);
+            else
+                [y,stdVal,maxVal,minVal,muci] = getExpCombineNatureFrequencyDatas(dataCombineStructCells{plotCount},natureFre(i),baseField);
+            end
+            [yUp,yDown] = getUpDownRang(y,stdVal,maxVal,minVal,muci,errorType,rang);
+            y = y(rang);
+            if isa(yFilterFunPtr,'function_handle')
+                [y,yUp,yDown]= yFilterFunPtr(y,yUp,yDown);
+            end
+            if strcmp(errorType,'none')
+                fh.plotHandle(plotCount,i) = plot(x,y...
+                    ,'color',getPlotColor(plotCount)...
+                    ,'Marker',plotMarker...
+                    ,'LineStyle',plotLineStyle);
+            else
+                [fh.plotHandle(plotCount,i),fh.errFillHandle(plotCount,i)] = plotWithError(x,y,yUp,yDown...
+                    ,'color',plotColor...
+                    ,'Marker',plotMarker...
+                    ,'LineStyle',plotLineStyle);
+            end
+        end
+    end
+    xlim([2,11]);
+    if ~isempty(legendLabels)
+        fh.legend = legend(fh.plotHandle,legendLabels,0);
+    end
+    set(gca,'Position',[0.13 0.18 0.79 0.65]);
+    fh.textboxTopAxixTitle = annotation('textbox',...
+        [0.48 0.885 0.0998 0.0912],...
+        'String','ÊµãÁÇπ',...
+        'FaceAlpha',0,...
+        'EdgeColor','none','FontName',paperFontName(),'FontSize',paperFontSize());
+    fh.textarrow = annotation('textarrow',[0.38 0.33],...
+        [0.744 0.665],'String',{'ÁºìÂÜ≤ÁΩê'},'FontName',paperFontName(),'FontSize',paperFontSize());
+    fh.vesselFillHandle = plotVesselRegion(gca,constExpVesselRangDistance());
+    ax = axis;
+    % ÁªòÂà∂ÊµãÁÇπÁ∫ø
+    yLabel2Detal = (ax(4) - ax(3))/12;
+    for i = 1:length(x)
+        fh.measurementGridLine(i) = plot([x(i),x(i)],[ax(3),ax(4)],':','color',[160,160,160]./255);
+        if 0 == mod(i,2)
+            continue;
+        end
+        if x(i) < 10
+            fh.measurementText(i) = text(x(i)-0.15,ax(4)+yLabel2Detal,sprintf('%d',i),'FontName',paperFontName(),'FontSize',paperFontSize());
+        else
+            fh.measurementText(i) = text(x(i)-0.3,ax(4)+yLabel2Detal,sprintf('%d',i),'FontName',paperFontName(),'FontSize',paperFontSize());           
+        end
+    end
+    xlabel('ÁÆ°Á∫øË∑ùÁ¶ª(m)');
+    ylabel('ÂÄçÈ¢ëÂπÖÂÄº(kPa)');
+end
+
+function fh = plotInBar(dataCombineStructCells,varargin)
+    pp = varargin;
+    errorType = 'ci';%ÁªòÂà∂ËØØÂ∑ÆÂ∏¶ÁöÑÊ®°ÂºèÔºåstdÔºömean+-sd,ci‰∏∫95%ÁΩÆ‰ø°Âå∫Èó¥Ôºåminmax‰∏∫ÊúÄÂ§ßÊúÄÂ∞è
+    rang = 1:13;
+    yFilterFunPtr = nan;
+    legendLabels = {};
+    baseField = 'rawData';
+    showPureVessel = 0;
+    rpm = 420;
+    natureFre= [1,2];%Âõ∫ÊúâÈ¢ëÁéáÔºåÊîØÊåÅ[0.5,1,1.5,2,2.5,3]
+    %ÂÖÅËÆ∏ÁâπÊÆäÁöÑÊääÂú∞‰∏Ä‰∏™varargin‰Ωú‰∏∫legend
+    if 0 ~= mod(length(pp),2)
+        legendLabels = pp{1};
+        pp=pp(2:end);
+    end
+    while length(pp)>=2
+        prop =pp{1};
+        val=pp{2};
+        pp=pp(3:end);
+        switch lower(prop)
+            case 'errortype' %ËØØÂ∑ÆÂ∏¶ÁöÑÁ±ªÂûã
+                errorType = val;
+            case 'rang'
+                rang = val;
+            case 'yfilterfunptr'
+                yFilterFunPtr = val;
+            case 'basefield'
+                baseField = val;
+            case 'naturefre'
+                natureFre = val;
+            case 'nf'
+                natureFre = val;
+            case 'showpurevessel'
+                showPureVessel = val;
+            case 'rpm'
+                rpm = val;
+            otherwise
+                error('ÂèÇÊï∞ÈîôËØØ%s',prop);
+        end
+    end
+
+    fh.figure = figure;
+    paperFigureSet_normal();
+    x = constExpMeasurementPointDistance();%ÊµãÁÇπÂØπÂ∫îÁöÑË∑ùÁ¶ª
     
-    hold on;
 end
-
-for plotCount = 1:length(dataCombineStructCells)
-    plotMarker = getMarkStyle(plotCount);
-    plotColor = getPlotColor(plotCount);
-    
-    for i=1:length(natureFre)
-        plotLineStyle = getLineStyle(i);
-        if 1==length(dataCombineStructCells)
-            [y,stdVal,maxVal,minVal,muci] = getExpCombineNatureFrequencyDatas(dataCombineStructCells,natureFre(i),baseField);
-        else
-            [y,stdVal,maxVal,minVal,muci] = getExpCombineNatureFrequencyDatas(dataCombineStructCells{plotCount},natureFre(i),baseField);
-        end
-        [yUp,yDown] = getUpDownRang(y,stdVal,maxVal,minVal,muci,errorType,rang);
-        y = y(rang);
-        if isa(yFilterFunPtr,'function_handle')
-            [y,yUp,yDown]= yFilterFunPtr(y,yUp,yDown);
-        end
-        if strcmp(errorType,'none')
-            fh.plotHandle(plotCount,i) = plot(x,y...
-                ,'color',getPlotColor(plotCount)...
-                ,'Marker',plotMarker...
-                ,'LineStyle',plotLineStyle);
-        else
-            [fh.plotHandle(plotCount,i),fh.errFillHandle(plotCount,i)] = plotWithError(x,y,yUp,yDown...
-                ,'color',plotColor...
-                ,'Marker',plotMarker...
-                ,'LineStyle',plotLineStyle);
-        end
-    end
-end
-xlim([2,11]);
-if ~isempty(legendLabels)
-    fh.legend = legend(fh.plotHandle,legendLabels,0);
-end
-set(gca,'Position',[0.13 0.18 0.79 0.65]);
-fh.textboxTopAxixTitle = annotation('textbox',...
-    [0.48 0.885 0.0998 0.0912],...
-    'String','≤‚µ„',...
-    'FaceAlpha',0,...
-    'EdgeColor','none','FontName',paperFontName(),'FontSize',paperFontSize());
-fh.textarrow = annotation('textarrow',[0.38 0.33],...
-    [0.744 0.665],'String',{'ª∫≥Âπﬁ'},'FontName',paperFontName(),'FontSize',paperFontSize());
-fh.vesselFillHandle = plotVesselRegion(gca,constExpVesselRangDistance());
-ax = axis;
-% ªÊ÷∆≤‚µ„œﬂ
-yLabel2Detal = (ax(4) - ax(3))/12;
-for i = 1:length(x)
-    fh.measurementGridLine(i) = plot([x(i),x(i)],[ax(3),ax(4)],':','color',[160,160,160]./255);
-    if 0 == mod(i,2)
-        continue;
-    end
-    if x(i) < 10
-        fh.measurementText(i) = text(x(i)-0.15,ax(4)+yLabel2Detal,sprintf('%d',i),'FontName',paperFontName(),'FontSize',paperFontSize());
-    else
-        fh.measurementText(i) = text(x(i)-0.3,ax(4)+yLabel2Detal,sprintf('%d',i),'FontName',paperFontName(),'FontSize',paperFontSize());           
-    end
-end
-xlabel('π‹œﬂæ‡¿Î(m)');
-ylabel('±∂∆µ∑˘÷µ(kPa)');
-
-end
-
-
