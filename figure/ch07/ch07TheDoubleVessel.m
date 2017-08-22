9077665e9534dc202253fe533bdb7c576b86ab41
mqy0615 <15001395919@163.com>
2017-08-22 21:56:08 +0800

    Signed-off-by: mqy0615 <15001395919@163.com>

--- figure/ch07/ch07TheDoubleVessel.m
+++ figure/ch07/ch07TheDoubleVessel.m
@@ -1,16 +1,16 @@
-%% 第七章 绘图 - 双罐罐二做弯头理论分析对比
+%% 第七章 绘图 - 双罐理论分析
 %第七章画图的参数设置
 clear all;
 % close all;
 clc;
 freRaw = [7,14,21,28,14*3];
 massFlowERaw = [0.02,0.2,0.03,0.003,0.007];
-theoryDataCells = doubleVesselBeElbowChangDistanceToFirstVessel('massflowdata',[freRaw;massFlowERaw]...
+theoryDataCells = doubleVesselChangDistanceToFirstVessel('massflowdata',[freRaw;massFlowERaw]...
     ,'meanFlowVelocity',14);
 plusValue = theoryDataCells(2:end,2);
 X = theoryDataCells(2:end,3);
-legendLabels = theoryDataCells(2:end,1);
 linkPipeLength = cell2mat(theoryDataCells(2:end,5));
+legendLabels = theoryDataCells(2:end,1);
 fh = figureTheoryPressurePlus(plusValue,X...
     ,'Y',linkPipeLength...
     ,'legendLabels',legendLabels...
