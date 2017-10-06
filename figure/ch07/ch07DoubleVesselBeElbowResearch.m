d00db74098834417121382a32a6728034eec597e
czyt1988 <czy.t@163.com>
2017-10-06 23:23:38 +0800

    Signed-off-by: czyt1988 <czy.t@163.com>

--- figure/ch07/ch07DoubleVesselBeElbowResearch.m
+++ figure/ch07/ch07DoubleVesselBeElbowResearch.m
@@ -1,17 +1,27 @@
 %% 第七章 绘图 - 双罐罐二做弯头理论分析对比
 %第七章画图的参数设置clear all;
-% close all;
+close all;
 clc;
 freRaw = [7,14,21,28,14*3];
 massFlowERaw = [0.02,0.2,0.03,0.003,0.007];
 %% 冲击脉动响应
 if 1
-    theoryDataCells = doubleVesselBeElbowShockResponse();
-    fh = plotSpectrumContourf(theoryDataCells{1, 2}.Mag',theoryDataCells{1, 2}.Fre',theoryDataCells{1,3});
+    figure
+    shockResponseBeElbowDataCells = doubleVesselBeElbowShockResponse();
+    fh = plotSpectrumContourf(shockResponseBeElbowDataCells{2, 2}.Mag'...
+        ,shockResponseBeElbowDataCells{2, 2}.Fre'...
+        ,shockResponseBeElbowDataCells{2,3});
+    xlim([0,100]);
+    
+    shockResponseStraingDataCells = doubleVesselShockResponse();
+    figure
+    fh = plotSpectrumContourf(shockResponseStraingDataCells{2, 2}.Mag'...
+        ,shockResponseStraingDataCells{2, 2}.Fre'...
+        ,shockResponseStraingDataCells{2,3});
     xlim([0,100]);
 end
 %% 改变第二个缓冲罐到第一个缓冲罐距离对脉动的影响
-if 1 % 改变第二个缓冲罐到第一个缓冲罐距离对脉动的影响
+if 0 % 改变第二个缓冲罐到第一个缓冲罐距离对脉动的影响
     theoryDataCells = doubleVesselBeElbowChangDistanceToFirstVessel('massflowdata',[freRaw;massFlowERaw]...
         ,'meanFlowVelocity',14);
     plusValue = theoryDataCells(2:end,2);
--- theory/doubleVessel/doubleVesselShockResponse.m
+++ theory/doubleVessel/doubleVesselShockResponse.m
@@ -1,5 +1,15 @@
-function theoryDataCells = doubleVesselBeElbowShockResponse(varargin)
-% 双罐第二个缓冲罐作为弯头的冲击脉动响应分析 - 频率特性分析
+function theoryDataCells = doubleVesselShockResponse(varargin)
+% 双罐串联冲击脉动响应分析 - 频率特性分析
+%计算管容管容的脉动
+%   massFlowE1 经过fft后的质量流量，直接对质量流量进行去直流fft
+%  长度 L1     l    Lv1   l   L2  l    Lv2   l     L3
+%              __________         __________
+%             |          |       |          |
+%  -----------|          |-------|          |-------------
+%             |__________|       |__________|  
+%  直径 Dpipe       Dv1    Dpipe       Dv2          Dpipe
+%  
+    %计算双罐串联
     pp = varargin;
     param.meanFlowVelocity = nan;
     while length(pp)>=2
@@ -42,9 +52,9 @@ function theoryDataCells = doubleVesselBeElbowShockResponse(varargin)
     param.notMach = 0;
 
     param.L1 = 3.5;%L1(m)
-    param.L2 = 1.5;%1.5;%双罐串联罐二作弯头两罐间距
-    param.L3 = 4;%4%双罐串联罐二作弯头出口管长
-
+    param.L2 = 4;%4%双罐串联接管长
+    param.L3 = 4;%4%双罐串联出口管长
+    
     param.Dpipe = 0.098;%管道直径（m）%应该是0.106
     param.l = 0.01;
     param.DV1 = 0.372;%缓冲罐的直径（m）
@@ -53,7 +63,6 @@ function theoryDataCells = doubleVesselBeElbowShockResponse(varargin)
     param.LV2 = 1.1;%variant_r(i).*param.DV2;%缓冲罐总长 （1.1m）
     param.Lv1 = param.LV1./2;%缓冲罐腔1总长
     param.Lv2 = param.LV1-param.Lv1;%缓冲罐腔2总长   
-    param.lv3 = 0.150+0.168;%针对单一偏置缓冲罐入口偏置长度
     param.Dbias = 0;%偏置管伸入罐体部分为0，所以对应直径为0
     param.sectionL1 = 0:0.25:param.L1;%[2.5,3.5];%0:0.25:param.L1
     param.sectionL2 = 0:0.25:param.L2;
@@ -77,34 +86,23 @@ function theoryDataCells = doubleVesselBeElbowShockResponse(varargin)
     dcpss.rp = 0.1;%边带区衰减DB数设置
     dcpss.rs = 30;%截止区衰减DB数设置
 
-%       长度 L1     l    Lv    l    L2   l    Lv
-    %                   __________            ___________ 
-    %                  |          |          |           |   
-    %       -----------|          |----------|           |
-    %                  |__________|          |__   ______|      
-    % 直径      Dpipe       Dv       Dpipe      | |
-    %                                           | | L3 
-    %                                           | |
-    %计算双罐-罐二作弯头 入口流速调节到45时与模拟较接近(L1,L3,L4,0.045)
-    %计算双罐-罐二作弯头 入口流速调节到45时与模拟较接近(L1,L3,L4,0.045)
-    count = 1;
     theoryDataCells{1,1} = '名称';
     theoryDataCells{1,2} = 'dataStrcutCell';
     theoryDataCells{1,3} = 'X';
     theoryDataCells{1,4} = 'param';
+    theoryDataCells{1,5} = '中间接管长';
     count = count + 1;
-   
-    [pressure1,pressure2,pressure3] = ...
-        doubleVesselElbowPulsationCalc(param.massFlowE,param.fre,shockTime,...
+   [pressure1,pressure2,pressure3] = ...
+        doubleVesselPulsationCalc(param.massFlowE,param.fre,shockTime,...
             param.L1,param.L2,param.L3,...
             param.LV1,param.LV2,param.l,param.Dpipe,param.DV1,param.DV2,...
-            param.lv3,param.Dbias,...
             param.sectionL1,param.sectionL2,param.sectionL3,...
-            'a',param.acousticVelocity,'isDamping',param.isDamping,'friction',0.045,...
+            'a',param.acousticVelocity,'isDamping',param.isDamping,'friction',0.003,...
             'meanFlowVelocity',param.meanFlowVelocity,'isUseStaightPipe',1,...
             'm',param.mach,'notMach',param.notMach...
             ,'isOpening',param.isOpening...
             );%,'coeffDamping',opt.coeffDamping
+    
      rawDataStruct = fun_dataProcessing([pressure1,pressure2,pressure3]...
             ,'fs',param.Fs...
             ,'basefrequency',baseFrequency...
@@ -114,10 +112,11 @@ function theoryDataCells = doubleVesselBeElbowShockResponse(varargin)
             ,'beforeAfterMeaPoint',beforeAfterMeaPoint...
             ,'calcpeakpeakvaluesection',calcPeakPeakValueSection...
             );
-    theoryDataCells{count,1} = sprintf('双罐罐二作弯头冲击响应');
+    theoryDataCells{count,1} = sprintf('双罐串联冲击响应');
     theoryDataCells{count,2} = rawDataStruct;
     theoryDataCells{count,3} = [param.sectionL1...
                                 ,param.L1+param.LV1+2*param.l+param.sectionL2...
-                                ,param.L1+param.LV1+2*param.l+param.L2+param.lv3+param.DV2/2+param.sectionL3];
+                                ,param.L1+param.LV1+2*param.l+param.L2+param.sectionL3];
     theoryDataCells{count,4} = param;
+    theoryDataCells{count,5} = param.L3;
 end
