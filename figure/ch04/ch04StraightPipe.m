654a9ebfc669f8090413c9fbd9ea9f46ec17c725
czyt1988 <czy.t@163.com>
2018-01-02 14:36:27 +0800

    修正bug

--- figure/ch04/ch04StraightPipe.m
+++ figure/ch04/ch04StraightPipe.m
@@ -30,16 +30,16 @@ if 1
 	rang = 1:13;
 	titleLabel = {'a','b','c','d','e','f','g','h','i','j','k','l','m'};
 	baseFre = 14;
+	baseFre1Amp = [];
+	baseFre1Time = [];
+	baseFre2Amp = [];
+	baseFre2Time = [];
 	for i=rang
 		figure
 		paperFigureSet('small',6);
-		pressure = dataCells.pressure(:,i);
+		pressure = straightPipeDataCells.pressure(:,i);
 		hold on;
 		[fh,sd,mag] = plotSTFT(pressure,STFT,Fs,'isShowColorbar',0,'chartType',chartType);
-		title(titleLabel{i},'FontName',paperFontName(),'FontSize',paperFontSize());
-		xlabel('频率(Hz)','FontName',paperFontName(),'FontSize',paperFontSize()); 
-		ylabel('时间(s)','FontName',paperFontName(),'FontSize',paperFontSize());
-		zlabel('幅值','FontName',paperFontName(),'FontSize',paperFontSize());
 		%查找1倍频和2倍频
 		x1f = zeros(1,size(mag,1));
 		y1f = x1f;
@@ -56,9 +56,64 @@ if 1
 		%标定1,2倍频
 		h = plot3(x1f,y1f,z1f,'-.b');
 		h = plot3(x2f,y2f,z2f,'-.b');
-		
+		baseFre1Amp(i,:) = z1f;
+		baseFre2Amp(i,:) = z2f;
+		baseFre1Time = sd.T;
+		baseFre2Time = baseFre1Time;
+		title(titleLabel{i},'FontName',paperFontName(),'FontSize',paperFontSize());
+		xlabel('频率(Hz)','FontName',paperFontName(),'FontSize',paperFontSize()); 
+		ylabel('时间(s)','FontName',paperFontName(),'FontSize',paperFontSize());
+		zlabel('幅值','FontName',paperFontName(),'FontSize',paperFontSize());
+		axis tight;
+		box on;
+		view(45,45);
 	end
-	
+	%绘制倍频
+	chartType = '2d';
+	%绘制所有1倍频
+	figure
+	paperFigureSet('normal',6);
+	if strcmpi(chartType,'2d')
+		hold on;
+		for i = 1:length(rang)
+			h = plot(baseFre1Time,baseFre1Amp(i,:),'color',getPlotColor(i),'marker',getMarkStyle(i));
+		end
+		box on;
+		xlabel('时间');
+		ylabel('幅值');
+	else
+		hold on;
+		for i = 1:length(rang)
+			h = plotSpectrum3(baseFre1Time,baseFre1Amp(i,:),rang(i),'isFill',1,'color',[229,44,77]./255);
+		end
+		xlabel('时间');
+		ylabel('测点');
+		zlabel('幅值');
+		axis tight;
+		box on;
+	end
+	%绘制所有2倍频
+	figure
+	paperFigureSet('normal',6);
+	if strcmpi(chartType,'2d')
+		hold on;
+		for i = 1:size(baseFre2Amp,1)
+			h = plot(baseFre2Time,baseFre2Amp(i,:),'color',getPlotColor(i),'marker',getMarkStyle(i));
+		end
+		box on;
+		xlabel('时间');
+		ylabel('幅值');
+	else
+		hold on;
+		for i = 1:length(rang)
+			h = plotSpectrum3(baseFre2Time,baseFre2Amp(i,:),rang(i),'isFill',1,'color',[229,44,77]./255);
+		end
+		xlabel('时间');
+		ylabel('测点');
+		zlabel('幅值');
+		axis tight;
+		box on;
+	end
 	
 	
     % dataNumIndex = 2;%读取的实验组数，<5
