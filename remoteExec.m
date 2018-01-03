85eef0dbcd1da86813d784fa2dbd647fa7d55941
czyt1988 <czy.t@163.com>
2018-01-03 14:09:36 +0800

    添加远程执行脚本

---
+++ remoteExec.m
@@ -0,0 +1,5 @@
+function remoteExec
+	%需要通过remoteRun运行的代码在这里修改，这样只要运行remoteRun，会先拉取最新的git分支，然后运行本函数
+	
+	ch04StraightPipe
+end
---
+++ remoteRun.m
@@ -0,0 +1,4 @@
+%%Ô¶³ÌÖ´ĞĞ´úÂë
+!git fetch origin master:matlab_tmp_remote_run
+!git merge matlab_tmp_remote_run
+remoteExec();
